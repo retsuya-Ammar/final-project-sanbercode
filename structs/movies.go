2aaff8c2c59e9a6b5df45e75d44ef05373246594
retsuya-Ammar <amarmaulana783@gmail.com>
2023-01-23 18:08:55 +0700

    fix typo

--- structs/movies.go
+++ structs/movies.go
@@ -8,7 +8,7 @@ type Movies struct {
 	Slug          string    `json:"slug"`
 	Category      string    `json:"category"`
 	Video_url     string    `json:"video_url"`
-	Thumbnail_url string    `json:"thumbnail_url"`
+	Thumbnail_url string    `json:"thumbnail"`
 	Rating        float64   `json:"rating"`
 	Is_featured   bool      `json:"is_featured"`
 	Created_at    time.Time `json:"created_at"`
